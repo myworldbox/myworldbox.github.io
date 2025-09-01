27b8acdccc0b05f526bfbe191f80f0840e897ba5
SKHHO\vchleung <54nicholasleung45@gmail.com>
2025-09-01 16:16:36 +0800

    fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d78cb7fa21f3aece71e66010fd33b8d9aec6b2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2855296582"
+    serviceWorkerVersion: "3322598866"
   }
 });
