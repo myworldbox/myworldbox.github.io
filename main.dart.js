99bf4c93b539cf43004a84f2b1bf622344436065
victor.leung <54nicholasleung45@gmail.com>
2026-07-06 12:53:50 +0800

    fix

--- main.dart.js
+++ main.dart.js
@@ -41404,7 +41404,7 @@ $S:3}
 A.bf0.prototype={
 $1(a){var s=A.ft().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/a10d8ac38de835021c8d2f920dbf50a920ccc030/":s)+a},
 $S:33}
 A.a4e.prototype={
 gu(a){var s=this.a
